34a9e9ac2ce1d831f10b2f4e41cfa6723e5b7392
mirlz <mirlzy@gmail.com>
2022-07-11 23:52:47 +0800

    test: add in test case for numbers,js

---
+++ basic-testing-frontend/src/util/numbers.spec.js
@@ -0,0 +1,36 @@
+import { describe, it, assert, expect } from 'vitest';
+
+import { transformToNumber } from './numbers';
+
+describe('transformToNumber function: ', () => {
+    it('should take in number and still return number', () => {
+        const number = 2;
+
+        const result = transformToNumber(number);
+
+        expect(result).toBeTypeOf('number');
+    })
+
+    it('should take in string literals and convert to number', () => {
+        const numberStr = '1';
+
+        const result = transformToNumber(numberStr);
+
+        expect(result).toBeTypeOf('number');
+        expect(result).toBe(1);
+    });
+
+    it('should return Nan if passed in value is invalid / unable to convert to number', () => {
+        const rando = 'some shit';
+
+        const result = transformToNumber(rando)
+
+        expect(result).toBeNaN();
+    });
+
+    it('should return Nan if nothing is passed in', () => {
+        const result = transformToNumber();
+
+        expect(result).toBeNaN();
+    });
+});
